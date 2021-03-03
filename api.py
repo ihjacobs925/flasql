c1592ec2e49cb7279d97860e08303e9840c107ee
ihjacobs925 <ihjacobs925@gmail.com>
2021-03-03 16:56:45 -0600

    Get users

--- api.py
+++ api.py
@@ -3,24 +3,23 @@ from flask import jsonify, request
 
 @app.route("/")
 def home():
-    return jsonify(message="Welcome to my api")
+  return jsonify(message="Welcome to my api")
 
 @app.route("/users", methods=["GET", "POST"])
 def user_index_create():
-    if request.method == "GET":
-        # Get all users
-        return jsonify([])
-    if request.method == "POST":
-        # create a user
-        # print(request.json)
-        # print(request.form)
-        # print(request.get_json)
-        new_user = User(name=request.form["name"], email=request.form["email"], bio=request.form["bio"])
-        db.session.add(new_user)
-        db.session.commit()
-        print(new_user)
-        return jsonify(new_user)
-    
+  if request.method == "GET":
+    # get all users
+    users = User.query.all()
+    print(type(users[0]))
+    results = [user.to_dict() for user in users]
+    return jsonify(results)
+  if request.method == "POST":
+    # create a user
+    new_user = User(name=request.form["name"], email=request.form["email"], bio=request.form["bio"])
+    db.session.add(new_user)
+    db.session.commit()
+    print(new_user)
+    return jsonify(new_user.to_dict())
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
--- models.py
+++ models.py
@@ -1,70 +1,70 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/flasql"
-# [DB_TYPE]+[DB_Connector]://[USERNAME]:[PASSWORD]@[HOST]:[PORT]/[DB_NAME]
+# [DB_TYPE]+[DB_CONNECTOR]://[USERNAME]:[PASSWORD]@[HOST]:[PORT]/[DB_NAME]
 db = SQLAlchemy(app)
 
-# -------- MODELS --------
+# ------- MODELS ------
 class User(db.Model):
-    __tablename__ = "users"
+  __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    name = db.Column(db.String, nullable=False)
-    bio = db.Column(db.String(150))
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String, unique=True, nullable=False)
+  name = db.Column(db.String, nullable=False)
+  bio = db.Column(db.String(150))
 
-    posts = db.relationship("Post", back_populates="author", lazy=True)
-    # posts = db.relationship("Post", backref="author", lazy=True)
+  posts = db.relationship("Post", back_populates="author", lazy=True)
+  # posts = db.relationship("Post", backref="author", lazy=True)
 
-    def __repr__(self):
-        return f"User(id={self.id}, email={self.email}, name={self.name}, bio={self.bio})"
-
-    def to_dict(self):
-        return {
-            id: self.id,
-            name: self.name,
-            email: self.email.
-            bio: self.bio
-        }
+  def __repr__(self):
+    return f"User(id={self.id}, email={self.email}, name={self.name}, bio={self.bio})"
+  
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "email": self.email,
+      "bio": self.bio
+    }
 
 post_tags = db.Table(
-    "post_tags",
-    db.Column("tag_id", db.Integer, db.ForeignKey('tags.id'), primary_key=True),
-    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+  "post_tags",
+  db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True),
+  db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True)
 )
 
 class Post(db.Model):
-    __tablename__ = "posts"
+  __tablename__ = "posts"
 
-    id = db.Column(db.Integer, primary_key=True)
-    header = db.Column(db.String(150), unique=True, nullable=False)
-    body = db.Column(db.String, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="SET NULL"))
+  id = db.Column(db.Integer, primary_key=True)
+  header = db.Column(db.String(150), unique=True, nullable=False)
+  body = db.Column(db.String, nullable=False)
+  author_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="SET NULL"))
 
-    author = db.relationship("User", back_populates="posts", lazy="subquery")
-    # author = db.relationship("User", backref="posts") need only on 1 class if used over back_populates
-    tags = db.relationship("Tag", back_populates="posts", lazy="subquery", secondary=post_tags)
+  author = db.relationship("User", back_populates="posts", lazy="subquery")
+  # author = db.relationship("User", backref="posts")
+  tags = db.relationship("Tag", back_populates="posts", lazy="subquery", secondary=post_tags)
 
-    def __repr__(self):
-        return f"Post(id={self.id}, header={self.header}, body={self.body}, author_id={self.author_id})"
+  def __repr__(self):
+    return f"Post(id={self.id}, header={self.header}, body={self.body}, author_id={self.author_id})"
 
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+  def as_dict(self):
+    return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class Tag(db.Model):
-    __tablename__ = "tags"
+  __tablename__ = "tags"
 
-    id = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.String(50), unique=True, nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  tag = db.Column(db.String(50), unique=True, nullable=False)
 
-    posts = db.relationship("Post", back_populates="tags", lazy=True, secondary=post_tags)
+  posts = db.relationship("Post", back_populates="tags", lazy=True, secondary=post_tags)
 
-    def __repr__(self):
-        return f"Tag(id={self.id}, tag={self.tag})"
+  def __repr__(self):
+    return f"Tag(id={self.id}, tag={self.tag})"
 
-    def as_dict(self):
-        return {id: self.id, tag: self.tag}
+  def as_dict(self):
+    return {id: self.id, tag: self.tag}
