fb2774c8eed66a49054d6fb2499d02c75e70b65d
ihjacobs925 <ihjacobs925@gmail.com>
2021-03-03 18:00:45 -0600

    delete

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from models import app, db, User
-from flask import jsonify, request
+from flask import jsonify, request, redirect, url_for
 
 @app.route("/")
 def home():
@@ -20,6 +20,40 @@ def user_index_create():
     db.session.commit()
     print(new_user)
     return jsonify(new_user.to_dict())
+
+@app.route("/users/<int:id>", methods=["GET", "PUT", "DELETE"])
+def user_show_update_delete(id):
+    if request.method == "GET":
+        # get the user
+        user = User.query.get(id)
+        if user:
+            return jsonify(user.to_dict())
+        else:
+            return jsonify(message="There is no user here")
+    if request.method == "PUT":
+        # get a user
+        user = User.query.get(id)
+        if user:
+        # update a user
+            # user.name = request.form["name"] or user.name
+            # user.email = request.form["email"] or user.email
+            # user.bio = request.form["bio"] or user.bio
+            for key, value in request.form.items():
+                print(f"@@@@ {key}: {value}")
+                setattr(user, key, value)
+            db.session.commit()
+            return jsonify(user.to_dict())
+        else:
+            return jsonify(message="There is no user here")
+    if request.method == "DELETE":
+        # Delete a user
+        user = User.query.get(id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify(message='Successful deletion')
+        else:
+            return jsonify(message="No user here")
 
 if __name__ == "__main__":
   app.run()
